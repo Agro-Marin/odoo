698e17ccfc8035b2536b07aaaffacad23cc6fff6
Josue Gonzalez <jg472470@gmail.com>
2026-03-20 22:00:12 -0600

    [FIX] stock_landed_costs: fix _prepare_aml_vals signature mismatch
    The override used positional `move=False` parameter and passed it to
    super(), but the base method in purchase.order.line uses `**optional_values`
    (kwargs). This caused TypeError when purchase called `_prepare_aml_vals()`
    without arguments.
    
    Align signature with base method to use **optional_values.
    
    Task ID: 19757

--- addons/stock_landed_costs/models/purchase.py
+++ addons/stock_landed_costs/models/purchase.py
@@ -4,7 +4,7 @@ from odoo import api,models
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
-    def _prepare_aml_vals(self, move=False):
-        res = super()._prepare_aml_vals(move)
+    def _prepare_aml_vals(self, **optional_values):
+        res = super()._prepare_aml_vals(**optional_values)
         res.update({'is_landed_costs_line': self.product_id.landed_cost_ok})
         return res
