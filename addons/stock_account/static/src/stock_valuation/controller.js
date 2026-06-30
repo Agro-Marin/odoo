3bd522eee05b85f53fa5c30af4afe1c48cd8dd6e
Luis Marin <marin.guadarrama@gmail.com>
2026-06-30 12:55:24 -0600

    [FIX] stock_account: port upstream 19.0 valuation core + reports
    Port upstream 19.0 stock_account fixes: valuation core (move value, COGS,
    account move/line, lot, company, product_value), AVCO rewrite (1af1947),
    to_date accuracy, move value read-only, post-init hook, valuation &
    AVCO-audit report accuracy.
    
    Note: _run_avco KeyError guard (H1) applied in a follow-up commit.
    
    Task ID: 23374

--- addons/stock_account/static/src/stock_valuation/controller.js
+++ addons/stock_account/static/src/stock_valuation/controller.js
@@ -2,7 +2,7 @@
 import { luxon } from "@web/core/l10n/luxon";
 import { reactive } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
-import { serializeDate, serializeDateTime } from "@web/core/l10n/dates";
+import { serializeDate } from "@web/core/l10n/dates";
 const { DateTime } = luxon;
 
 
@@ -46,7 +46,6 @@ export class StockValuationReportController {
         }
         // Prepare the "Initial Balance" lines.
         this.data.initial_balance.lines = [];
-        this.data.initial_balance.accounts = [];
         for (let [accountId, data] of Object.entries(this.data.initial_balance.lines_by_account_id)) {
             const account = this.data.accounts_by_id[accountId];
             this.data.initial_balance.lines.push({
@@ -54,11 +53,9 @@ export class StockValuationReportController {
                 value: data.value,
                 account_id: accountId,
             });
-            this.data.initial_balance.accounts.push(...data.accounts);
         }
         // Prepare the "Ending Stock" lines.
         this.data.ending_stock.lines = [];
-        this.data.ending_stock.accounts = [];
         for (let [accountId, data] of Object.entries(this.data.ending_stock.lines_by_account_id)) {
             const account = this.data.accounts_by_id[accountId];
             this.data.ending_stock.lines.push({
@@ -66,13 +63,12 @@ export class StockValuationReportController {
                 value: data.value,
                 account_id: accountId,
             });
-            this.data.ending_stock.accounts.push(...data.accounts);
         }
     }
 
     async setDate(date) {
         this.state.date = date;
-        this.dateAsString = serializeDateTime(date);
+        this.dateAsString = serializeDate(date);
         await this.loadReportData();
     }
 
