Agro-Marin/odoo

commit 07a333c85636dc2b4ad76d8071e6f3b38c492810

[FIX] base: ir_qweb concurrent access
Ensure it is not possible to render qweb content concurrently

odoo/addons/base/models/ir_qweb.py
@@ -616,12 +616,21 @@ class QwebContent:
     params__: QwebCallParameters  # not available for the python expression inside the xml
 
     def __init__(self, irQweb: IrQweb, params: QwebCallParameters):
-        self.irQweb = irQweb
+        self.__irQweb = irQweb
         self.html = None
         self.params__ = params
 
+    @property
+    def irQweb(self):
+        irQweb = self.__irQweb
+        if threading.current_thread().dbname != irQweb.env.cr.dbname:
+            return None
+        return irQweb
+
     def __str__(self):
         if self.html is None:
+            if self.irQweb is None:
+                return ''
             params = self.params__
             self.html = ''.join(self.irQweb._render_iterall(
                params.view_ref, params.method, params.values, params.directive,

commit 49b312f5c76f5ab998295c7dd57eda4f11f048ba

[FIX] qweb: qwebcontent concurrent access
Allow concurrent access to ir_qweb content outside of normal http/cron
workers. Commit 07a333c856 introduced a regression for script users that
are not affected by concurrency bug

Reported in #271844

odoo/addons/base/models/ir_qweb.py
@@ -623,7 +623,8 @@ class QwebContent:
     @property
     def irQweb(self):
         irQweb = self.__irQweb
-        if threading.current_thread().dbname != irQweb.env.cr.dbname:
+        thread_dbname = getattr(threading.current_thread(), 'dbname', None)
+        if thread_dbname and thread_dbname != irQweb.env.cr.dbname:
             return None
         return irQweb
 