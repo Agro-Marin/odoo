577d98cb94608f58cd328f01923a7119c5cb226b
x <x@x>
2026-07-02 06:29:20 -0600

    cleaunup

--- odoo/addons/test_http/tests/__init__.py
+++ odoo/addons/test_http/tests/__init__.py
@@ -4,7 +4,6 @@ from . import test_device
 from . import test_echo_reply
 from . import test_error
 from . import test_greeting
-from . import test_http_audit
 from . import test_misc
 from . import test_models
 from . import test_openapi
