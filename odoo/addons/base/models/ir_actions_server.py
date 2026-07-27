524ce9cba577676505ace870370e30c7ab0431ed
Luis Marin <marin.guadarrama@gmail.com>
2026-07-27 01:18:26 -0600

    Revert "[FIX] base, orm, web: enforce ir.actions ondelete the ORM cannot delegate to PostgreSQL"
    This reverts commit b2e1a587ba24afe384fcaa542c26a12fd287fe06.

--- odoo/addons/base/models/ir_actions_server.py
+++ odoo/addons/base/models/ir_actions_server.py
@@ -805,7 +805,7 @@ class IrActionsServer(models.Model):
                 )
             )
 
-    def _get_readable_fields(self) -> frozenset[str]:
+    def _get_readable_fields(self) -> set[str]:
         return super()._get_readable_fields() | {
             "group_ids",
             "model_name",
