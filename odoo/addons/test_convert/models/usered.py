d96c86f372f18d1b4f04b5b167c89000233ac2e6
JemXiaoLong <escuderojem@outlook.com>
2026-08-26 22:14:00 -0600

    [REF] test_convert: split models.py into models/ package
    test_convert declared two model classes (Test_ConvertTest_Model,
    Test_ConvertUsered) in one flat models.py, unlike the fork's own
    convention: doc/coding_guidelines.rst SS1.3 "A second model class is
    a second file, whatever its size".
    
    Solution:
    - Split into models/test_model.py (Test_ConvertTest_Model),
      models/usered.py (Test_ConvertUsered), models/__init__.py
      importing both in dependency order.
    - No behaviour change.
    
    Verification:
    - odoo-bin -d <db> -i test_convert --test-enable --test-tags
      /test_convert --stop-after-init --workers=0 --addons-path=odoo/addons
      -> 0 failed, 0 error(s) of 23 tests.
    
    Task ID: 26101

--- odoo/addons/test_convert/models/usered.py
+++ odoo/addons/test_convert/models/usered.py
@@ -1,26 +1,6 @@
 from odoo import api, fields, models
 
 
-class Test_ConvertTest_Model(models.Model):
-    _name = "test_convert.test_model"
-    _description = "Test Convert Model"
-
-    name = fields.Char(translate=True)
-    usered_ids = fields.One2many("test_convert.usered", "test_id")
-
-    @api.model
-    def action_test_date(self, today_date):
-        return True
-
-    @api.model
-    def action_test_time(self, cur_time):
-        return True
-
-    @api.model
-    def action_test_timezone(self, timezone):
-        return True
-
-
 class Test_ConvertUsered(models.Model):
     _name = "test_convert.usered"
     _description = "z test model ignore"
