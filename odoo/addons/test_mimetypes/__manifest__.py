3ba0a5ffff64800df7ce2589e71a1773a6891514
JemXiaoLong <escuderojem@outlook.com>
2026-08-27 05:17:55 -0600

    [CLN] test_mimetypes: fix wrong manifest description
    The manifest description read "A module to generate exceptions.",
    copy-pasted boilerplate unrelated to this module -- it has nothing to
    do with generating exceptions, it tests MIME-type guessing.
    
    Solution:
    - Correct the description to describe what the module actually tests.
    
    Task ID: 26282

--- odoo/addons/test_mimetypes/__manifest__.py
+++ odoo/addons/test_mimetypes/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "test mimetypes-guessing",
     "version": "0.1",
     "category": "Hidden/Tests",
-    "description": """A module to generate exceptions.""",
+    "description": """A module to test MIME-type guessing.""",
     "author": "Odoo S.A.",
     "license": "LGPL-3",
 }
