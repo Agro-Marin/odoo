08872f7962f9df886d9c7ee44c27a6c32e772272
JemXiaoLong <escuderojem@outlook.com>
2026-08-27 14:41:01 -0600

    [LINT] cloud_storage_google: resolve ruff debt (UP017, F401, RET503)
    The file had never been run through ruff format/check: single-quote
    strings throughout, two implicit-None returns.
    
    Solution:
    - ruff format (whitespace/quote-style/wrapping only).
    - UP017: datetime.timezone.utc -> datetime.UTC alias; drop the now-unused
      timezone import (F401).
    - RET503 (x2): add explicit `return None` at the end of
      _setup_cloud_storage_provider and _check_cloud_storage_uninstallable,
      which already fell through returning None implicitly.
    
    No docstring or comment content touched here; a follow-up [CLN] commit
    handles the class docstring.
    
    Task ID: 26614

--- addons/cloud_storage_google/models/res_config_settings.py
+++ addons/cloud_storage_google/models/res_config_settings.py
@@ -1,12 +1,12 @@
 import base64
 import json
-import requests
-from datetime import datetime, timezone
+from datetime import UTC, datetime
 
+import requests
 from google.auth.transport.requests import Request
 
-from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError, UserError
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError, ValidationError
 
 from .ir_attachment import get_cloud_storage_google_credential
 
@@ -18,93 +18,137 @@ class ResConfigSettings(models.TransientModel):
         are still in use, you should promise the current service account
         has the permission to access the old bucket.
     """
-    _inherit = 'res.config.settings'
 
-    cloud_storage_provider = fields.Selection(selection_add=[('google', 'Google Cloud Storage')])
+    _inherit = "res.config.settings"
+
+    cloud_storage_provider = fields.Selection(
+        selection_add=[("google", "Google Cloud Storage")]
+    )
 
     cloud_storage_google_bucket_name = fields.Char(
-        string='Google Bucket Name',
-        config_parameter='cloud_storage_google_bucket_name')
+        string="Google Bucket Name", config_parameter="cloud_storage_google_bucket_name"
+    )
     # Google Service Account Key in JSON format
     cloud_storage_google_service_account_key = fields.Binary(
-        string='Google Service Account Key', store=False
+        string="Google Service Account Key", store=False
     )
     cloud_storage_google_account_info = fields.Char(
-        string='Google Service Account Info',
-        compute='_compute_cloud_storage_google_account_info',
+        string="Google Service Account Info",
+        compute="_compute_cloud_storage_google_account_info",
         store=True,
         readonly=False,
-        config_parameter='cloud_storage_google_account_info',
+        config_parameter="cloud_storage_google_account_info",
     )
 
     @api.model
     def get_values(self):
         res = super().get_values()
-        if account_info := self.env['ir.config_parameter'].get_param('cloud_storage_google_account_info'):
-            res['cloud_storage_google_service_account_key'] = base64.b64encode(account_info.encode())
+        if account_info := self.env["ir.config_parameter"].get_param(
+            "cloud_storage_google_account_info"
+        ):
+            res["cloud_storage_google_service_account_key"] = base64.b64encode(
+                account_info.encode()
+            )
         return res
 
-    @api.onchange('cloud_storage_google_service_account_key')
+    @api.onchange("cloud_storage_google_service_account_key")
     def _compute_cloud_storage_google_account_info(self):
         for setting in self:
-            key = setting.with_context(bin_size=False).cloud_storage_google_service_account_key
-            setting.cloud_storage_google_account_info = base64.b64decode(key) if key else False
+            key = setting.with_context(
+                bin_size=False
+            ).cloud_storage_google_service_account_key
+            setting.cloud_storage_google_account_info = (
+                base64.b64decode(key) if key else False
+            )
 
     def _setup_cloud_storage_provider(self):
-        ICP = self.env['ir.config_parameter']
-        if ICP.get_param('cloud_storage_provider') != 'google':
+        ICP = self.env["ir.config_parameter"]
+        if ICP.get_param("cloud_storage_provider") != "google":
             return super()._setup_cloud_storage_provider()
         # check bucket access
-        bucket_name = ICP.get_param('cloud_storage_google_bucket_name')
+        bucket_name = ICP.get_param("cloud_storage_google_bucket_name")
         # use different blob names in case the credentials are allowed to
         # overwrite an existing blob created by previous tests
-        blob_name = f'0/{datetime.now(timezone.utc)}.txt'
+        blob_name = f"0/{datetime.now(UTC)}.txt"
 
-        IrAttachment = self.env['ir.attachment']
+        IrAttachment = self.env["ir.attachment"]
         # check blob create permission
-        upload_url = IrAttachment._generate_cloud_storage_google_signed_url(bucket_name, blob_name, method='PUT', expiration=IrAttachment._cloud_storage_upload_url_time_to_expiry)
-        upload_response = requests.put(upload_url, data=b'', timeout=5)
+        upload_url = IrAttachment._generate_cloud_storage_google_signed_url(
+            bucket_name,
+            blob_name,
+            method="PUT",
+            expiration=IrAttachment._cloud_storage_upload_url_time_to_expiry,
+        )
+        upload_response = requests.put(upload_url, data=b"", timeout=5)
         if upload_response.status_code != 200:
-            raise ValidationError(_('The account info is not allowed to upload blobs to the bucket.\n%s', str(upload_response.text)))
+            raise ValidationError(
+                _(
+                    "The account info is not allowed to upload blobs to the bucket.\n%s",
+                    str(upload_response.text),
+                )
+            )
 
         # check blob read permission
-        download_url = IrAttachment._generate_cloud_storage_google_signed_url(bucket_name, blob_name, method='GET', expiration=IrAttachment._cloud_storage_download_url_time_to_expiry)
+        download_url = IrAttachment._generate_cloud_storage_google_signed_url(
+            bucket_name,
+            blob_name,
+            method="GET",
+            expiration=IrAttachment._cloud_storage_download_url_time_to_expiry,
+        )
         download_response = requests.get(download_url, timeout=5)
         if download_response.status_code != 200:
-            raise ValidationError(_('The account info is not allowed to download blobs from the bucket.\n%s', str(upload_response.text)))
+            raise ValidationError(
+                _(
+                    "The account info is not allowed to download blobs from the bucket.\n%s",
+                    str(upload_response.text),
+                )
+            )
 
         # CORS management is not allowed in the Google Cloud console.
         # configure CORS on bucket to allow .pdf preview and direct upload
-        cors = [{
-            'origin': ['*'],
-            'method': ['GET', 'PUT'],
-            'responseHeader': ['Content-Type'],
-            'maxAgeSeconds': IrAttachment._cloud_storage_download_url_time_to_expiry,
-        }]
-        credential = get_cloud_storage_google_credential(self.env).with_scopes(['https://www.googleapis.com/auth/devstorage.full_control'])
+        cors = [
+            {
+                "origin": ["*"],
+                "method": ["GET", "PUT"],
+                "responseHeader": ["Content-Type"],
+                "maxAgeSeconds": IrAttachment._cloud_storage_download_url_time_to_expiry,
+            }
+        ]
+        credential = get_cloud_storage_google_credential(self.env).with_scopes(
+            ["https://www.googleapis.com/auth/devstorage.full_control"]
+        )
         credential.refresh(Request())
         url = f"https://storage.googleapis.com/storage/v1/b/{bucket_name}?fields=cors"
         headers = {
-            'Authorization': f'Bearer {credential.token}',
-            'Content-Type': 'application/json'
+            "Authorization": f"Bearer {credential.token}",
+            "Content-Type": "application/json",
         }
-        data = json.dumps({'cors': cors})
+        data = json.dumps({"cors": cors})
         patch_response = requests.patch(url, data=data, headers=headers, timeout=5)
         if patch_response.status_code != 200:
-            raise ValidationError(_("The account info is not allowed to set the bucket's CORS.\n%s", str(patch_response.text)))
+            raise ValidationError(
+                _(
+                    "The account info is not allowed to set the bucket's CORS.\n%s",
+                    str(patch_response.text),
+                )
+            )
+        return None
 
     def _get_cloud_storage_configuration(self):
-        ICP = self.env['ir.config_parameter'].sudo()
-        if ICP.get_param('cloud_storage_provider') != 'google':
+        ICP = self.env["ir.config_parameter"].sudo()
+        if ICP.get_param("cloud_storage_provider") != "google":
             return super()._get_cloud_storage_configuration()
         configuration = {
-            'bucket_name': ICP.get_param('cloud_storage_google_bucket_name'),
-            'account_info': ICP.get_param('cloud_storage_google_account_info'),
+            "bucket_name": ICP.get_param("cloud_storage_google_bucket_name"),
+            "account_info": ICP.get_param("cloud_storage_google_account_info"),
         }
         return configuration if all(configuration.values()) else {}
 
     def _check_cloud_storage_uninstallable(self):
-        if self.env['ir.config_parameter'].get_param('cloud_storage_provider') != 'google':
+        if (
+            self.env["ir.config_parameter"].get_param("cloud_storage_provider")
+            != "google"
+        ):
             return super()._check_cloud_storage_uninstallable()
         cr = self.env.cr
         cr.execute(
@@ -117,4 +161,9 @@ class ResConfigSettings(models.TransientModel):
             """
         )
         if cr.fetchone():
-            raise UserError(_('Some Google attachments are in use, please migrate cloud storages before disable the provider'))
+            raise UserError(
+                _(
+                    "Some Google attachments are in use, please migrate cloud storages before disable the provider"
+                )
+            )
+        return None
