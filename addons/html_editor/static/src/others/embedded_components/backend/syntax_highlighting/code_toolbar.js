ca9fed7662e727fefb033f45fd2144a9bb09f134
JemXiaoLong <escuderojem@outlook.com>
2026-08-29 13:33:22 -0600

    [FIX] html_editor: make code-block language names translatable
    LANGUAGES display names in CodeToolbar were plain string literals with no
    _t() wrapper, unlike the rest of the module's user-facing strings -- they
    render directly as dropdown labels via t-esc/title in the template.
    
    Solution:
    - Wrap each display name in _t()
    
    Task ID: 28137

--- addons/html_editor/static/src/others/embedded_components/backend/syntax_highlighting/code_toolbar.js
+++ addons/html_editor/static/src/others/embedded_components/backend/syntax_highlighting/code_toolbar.js
@@ -2,24 +2,25 @@
 import { Component } from "@odoo/owl";
 import { CopyButton } from "@web/components/copy_button";
 import { Dropdown, DropdownItem } from "@web/components/dropdown";
+import { _t } from "@web/core/translation";
 
 export const LANGUAGES = {
-    plaintext: "Plain Text",
-    markdown: "Markdown",
-    javascript: "Javascript",
-    typescript: "Typescript",
-    jsdoc: "JSDoc",
-    java: "Java",
-    python: "Python",
-    html: "HTML",
-    xml: "XML",
-    svg: "SVG",
-    json: "JSON",
-    css: "CSS",
-    sass: "SASS",
-    scss: "SCSS",
-    sql: "SQL",
-    diff: "Diff",
+    plaintext: _t("Plain Text"),
+    markdown: _t("Markdown"),
+    javascript: _t("Javascript"),
+    typescript: _t("Typescript"),
+    jsdoc: _t("JSDoc"),
+    java: _t("Java"),
+    python: _t("Python"),
+    html: _t("HTML"),
+    xml: _t("XML"),
+    svg: _t("SVG"),
+    json: _t("JSON"),
+    css: _t("CSS"),
+    sass: _t("SASS"),
+    scss: _t("SCSS"),
+    sql: _t("SQL"),
+    diff: _t("Diff"),
 };
 
 export class CodeToolbar extends Component {
