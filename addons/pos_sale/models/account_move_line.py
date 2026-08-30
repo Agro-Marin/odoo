8c6ed063a98d4158b1aa5b9d9a8aa8257aaa9084
JemXiaoLong <escuderojem@outlook.com>
2026-08-30 03:11:27 -0600

    [LINT] pos_sale: ruff-format account_move_line.py
    Pure formatting: double quotes. AST-identical to the pre-format file
    (verified). Needed to land a clean F04 fix next without dragging
    unrelated reformatting into that diff.
    
    Task ID: 28534

--- addons/pos_sale/models/account_move_line.py
+++ addons/pos_sale/models/account_move_line.py
@@ -3,12 +3,12 @@ from odoo.tools import float_compare
 
 
 class AccountMoveLine(models.Model):
-    _inherit = 'account.move.line'
+    _inherit = "account.move.line"
 
     def _get_downpayment_lines(self):
         # EXTENDS sale
         downpayment_lines = self.env["account.move.line"]
-        if not self.env['pos.order.line'].has_access('read'):
+        if not self.env["pos.order.line"].has_access("read"):
             return super()._get_downpayment_lines()
 
         for record in self:
@@ -17,10 +17,19 @@ class AccountMoveLine(models.Model):
                 # if order is not settled through POS
                 # We're assuming that if an order is settled through POS it will have sale_line_ids empty.
                 # We're also assuming that the downpayment line will have the same price_subtotal & tax_ids as the record.
-                pos_downpayment_moves = related_sol.filtered("is_downpayment").pos_order_line_ids.order_id.account_move
+                pos_downpayment_moves = related_sol.filtered(
+                    "is_downpayment"
+                ).pos_order_line_ids.order_id.account_move
                 downpayment_lines |= pos_downpayment_moves.invoice_line_ids.filtered(
-                    lambda r, record=record, rounding=rounding: float_compare(r.price_subtotal, -record.price_subtotal, precision_rounding=rounding) == 0
-                    and r.tax_ids == record.tax_ids,
+                    lambda r, record=record, rounding=rounding: (
+                        float_compare(
+                            r.price_subtotal,
+                            -record.price_subtotal,
+                            precision_rounding=rounding,
+                        )
+                        == 0
+                        and r.tax_ids == record.tax_ids
+                    ),
                 )
 
             elif related_posl := record.move_id.pos_order_ids.lines:
@@ -28,17 +37,34 @@ class AccountMoveLine(models.Model):
                 # We get the downpayment lines through:
                 # final invoice -> final POS order -> origin sale order -> downpayment pos order -> downpayment invoice
                 sale_orders = related_posl.sale_order_origin_id
-                candidate_moves = sale_orders.pos_order_line_ids.order_id.account_move.filtered(lambda r: r._is_downpayment())
+                candidate_moves = (
+                    sale_orders.pos_order_line_ids.order_id.account_move.filtered(
+                        lambda r: r._is_downpayment()
+                    )
+                )
                 applicable_lines = candidate_moves.invoice_line_ids.filtered(
-                    lambda line, record=record, rounding=rounding: float_compare(line.price_subtotal, -record.price_subtotal, precision_rounding=rounding) == 0
-                    and line.tax_ids == record.tax_ids,
+                    lambda line, record=record, rounding=rounding: (
+                        float_compare(
+                            line.price_subtotal,
+                            -record.price_subtotal,
+                            precision_rounding=rounding,
+                        )
+                        == 0
+                        and line.tax_ids == record.tax_ids
+                    ),
                 )
 
                 if len(applicable_lines) > 1:
                     # In the case there are multiple downpayment lines with the same tax & total we'll
                     # pair them up as per their ids and get the downpayment line paired with the record.
                     move_lines = record.move_id.invoice_line_ids
-                    lines_dict = dict(zip(move_lines.sorted('id'), applicable_lines.sorted('id'), strict=False))
+                    lines_dict = dict(
+                        zip(
+                            move_lines.sorted("id"),
+                            applicable_lines.sorted("id"),
+                            strict=False,
+                        )
+                    )
                     downpayment_lines |= lines_dict.get(record)
                 else:
                     downpayment_lines |= applicable_lines
