fd56dafb80ee8ae3a05ecea7b0f6f8a2dd463b75
JemXiaoLong <escuderojem@outlook.com>
2026-08-30 17:25:20 -0600

    [CLN] resource: remove the unused resource.task HOOT mock model
    ResourceTask was registered in every call to defineResourceModels(),
    but no test file (resource, resource_mail) references resource.task,
    ResourceTask, or its fields. Dead test fixture code.
    
    Task ID: 28829

--- addons/resource/static/tests/resource_test_helpers.js
+++ addons/resource/static/tests/resource_test_helpers.js
@@ -1,10 +1,8 @@
 import { defineModels, webModels } from "@web/../tests/web_test_helpers";
 
 import { ResourceResource } from "./mock_server/mock_models/resource_resource.js";
-import { ResourceTask } from "./mock_server/mock_models/resource_task.js";
 
 export const resourceModels = {
-    ResourceTask,
     ResourceResource,
 };
 
