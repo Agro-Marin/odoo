1a86b82d2bcfd9aa1ff975d4fcb72d8a5b8a72c5
JemXiaoLong <escuderojem@outlook.com>
2026-08-30 18:11:42 -0600

    [LINT] resource_mail: ruff-format resource_resource.py
    Quote-style only, no behaviour change.
    
    Task ID: 28845

--- addons/resource_mail/models/resource_resource.py
+++ addons/resource_mail/models/resource_resource.py
@@ -2,9 +2,9 @@ from odoo import fields, models
 
 
 class ResourceResource(models.Model):
-    _inherit = 'resource.resource'
+    _inherit = "resource.resource"
 
-    im_status = fields.Char(related='user_id.im_status')
+    im_status = fields.Char(related="user_id.im_status")
 
     def get_avatar_card_data(self, fields):
         return self.read(fields)
