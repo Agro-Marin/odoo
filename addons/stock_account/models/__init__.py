a7f14a5cc51cc114ab81ae71bfb3439af47be097
JemXiaoLong <escuderojem@outlook.com>
2026-09-01 01:06:30 -0600

    [REM] stock_account: remove dead duplicate country_code field
    stock.picking.type.country_code duplicated stock.picking.country_code
    (identical related="company_id.account_fiscal_country_id.code") with
    zero consumers. Repo-wide grep across core, enterprise, and
    agromarin-addons found no reference to picking_type_id.country_code
    beyond the field's own declaration.
    
    Task ID: 29426

--- addons/stock_account/models/__init__.py
+++ addons/stock_account/models/__init__.py
@@ -10,6 +10,5 @@ from . import stock_location
 from . import stock_lot
 from . import stock_move_line
 from . import stock_picking
-from . import stock_picking_type
 from . import stock_quant
 from . import res_config_settings
--- addons/stock_account/models/stock_picking_type.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class StockPickingType(models.Model):
-    _inherit = "stock.picking.type"
-
-    country_code = fields.Char(related="company_id.account_fiscal_country_id.code")
