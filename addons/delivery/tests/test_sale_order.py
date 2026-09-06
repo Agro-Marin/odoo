522ec7c531df07c76b23b0809499161033e9314e
JemXiaoLong <escuderojem@outlook.com>
2026-09-06 04:39:46 -0600

    [LINT] delivery: ruff-format test_sale_order.py
    Quote-style and blank-line only, ahead of adding a new test to this
    file. Verified AST-identical.
    
    Task ID: 31114

--- addons/delivery/tests/test_sale_order.py
+++ addons/delivery/tests/test_sale_order.py
@@ -3,10 +3,11 @@ from odoo.tests import tagged
 from odoo.addons.sale.tests.common import SaleCommon
 
 
-@tagged('post_install', '-at_install')
+@tagged("post_install", "-at_install")
 class TestSaleOrder(SaleCommon):
-
     def test_avoid_setting_pickup_location_as_default_delivery_address(self):
-        self._create_partner(type='delivery', parent_id=self.partner.id, is_pickup_location=True)
-        so = self.env['sale.order'].create({'partner_id': self.partner.id})
+        self._create_partner(
+            type="delivery", parent_id=self.partner.id, is_pickup_location=True
+        )
+        so = self.env["sale.order"].create({"partner_id": self.partner.id})
         self.assertFalse(so.partner_shipping_id.is_pickup_location)
