aad84a3711cccb05ba4d70bc9b559989c75b8f1a
JemXiaoLong <escuderojem@outlook.com>
2026-09-07 07:40:37 -0600

    [FIX] loyalty: format expiration date in portal card dialog
    The dialog rendered card.expiration_date as a raw unformatted value,
    inconsistent with the loyalty history portal page which formats the
    equivalent date field with the datetime/short widget. Add a
    formattedExpirationDate getter using the same deserializeDate/
    formatDate helpers already used elsewhere in the codebase.
    
    Task ID: 31439

--- addons/loyalty/static/src/js/portal/loyalty_card_dialog/loyalty_card_dialog.js
+++ addons/loyalty/static/src/js/portal/loyalty_card_dialog/loyalty_card_dialog.js
@@ -1,9 +1,14 @@
 /** @odoo-module native */
 import { Component } from "@odoo/owl";
 import { Dialog } from "@web/ui/dialog";
+import { deserializeDate, formatDate } from "@web/core/l10n/dates";
 
 export class PortalLoyaltyCardDialog extends Component {
     static components = { Dialog };
     static template = "loyalty.portal_loyalty_card_dialog";
     static props = ["*"];
+
+    get formattedExpirationDate() {
+        return formatDate(deserializeDate(this.props.card.expiration_date));
+    }
 }
